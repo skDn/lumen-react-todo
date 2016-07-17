7f99577382b47c524a54fba23caacedc944b7903
skDn <2038011y@student.gla.ac.uk>
2016-07-18 01:05:13 +0300

    Users and Comments migration

---
+++ database/migrations/2016_07_17_192156_create_comments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('task_id');
+            $table->bigInteger('user_id');
+            $table->text('comment');
+            $table->timestamps();
+            $table->foreign('task_id')
+                ->references('id')
+                ->on('tasks');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('comments');
+    }
+}
